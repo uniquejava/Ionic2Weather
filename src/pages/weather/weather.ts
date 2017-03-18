18a302aa280d705e6c876501de85a7153bd6cffb
cyper <345343747@qq.com>
2017-03-19 00:23:30 +0800

    get your geo location

--- src/pages/weather/weather.ts
+++ src/pages/weather/weather.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, LoadingController, Refresher } from 'ionic-angular';
 import {WeatherService} from '../../providers/weather-service';
+import {Geolocation} from 'ionic-native';
+import {CurrentLoc} from '../../interfaces/current-loc';
+
 /*
   Generated class for the Weather page.
 
@@ -17,6 +20,7 @@ export class WeatherPage {
   daily: any = {};
   loader: LoadingController; // not needed at all
   refresher: Refresher; // not needed at all
+  currentLoc: CurrentLoc = {lat: 0, lon: 0};
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public weatherService: WeatherService, public loadingCtrl: LoadingController) {
     let loader = this.loadingCtrl.create({
@@ -24,6 +28,14 @@ export class WeatherPage {
       duration: 3000
     });
     loader.present();
+
+    Geolocation.getCurrentPosition().then(pos => {
+      console.log('lat: ', pos.coords.latitude, ', lon: ', pos.coords.longitude);
+      this.currentLoc.lat = pos.coords.latitude;
+      this.currentLoc.lon = pos.coords.longitude;
+      this.currentLoc.timestamp = pos.timestamp;
+    });
+
     this.weatherService.getWeather().then(theResult => {
       this.theWeather = theResult;
       this.currentData = this.theWeather.currently;
